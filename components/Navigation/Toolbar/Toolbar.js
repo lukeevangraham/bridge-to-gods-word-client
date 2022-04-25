a03f990f46b368b88758b835538f4c782e6423cd
Cefalu Wave <luke.evan.graham@gmail.com>
2022-04-25 00:45:38 -0700

    apostrophe

--- components/Navigation/Toolbar/Toolbar.js
+++ components/Navigation/Toolbar/Toolbar.js
@@ -3,7 +3,7 @@ import classes from "./Toolbar.module.scss";
 const Toolbar = ({ children }) => (
   <div className={classes.toolbar}>
     <div className={classes.toolbar__inner}>
-      <div>Bridge To God's Word</div>
+      <div>Bridge To God&apos;s Word</div>
       {children}
     </div>
   </div>
