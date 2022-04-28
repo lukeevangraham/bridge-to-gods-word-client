be9964c70a38331f54542c0e490790a4e0f52262
Luke Graham <luke.evan.graham@gmail.com>
2022-04-28 08:35:40 -0700

    open sans font

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import "@fontsource/lato/100.css"
-import "@fontsource/lato/300.css"
-import "@fontsource/lato/400.css"
-import "@fontsource/lato/700.css"
-import "@fontsource/lato/900.css"
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/500.css"
+import "@fontsource/open-sans/600.css"
+import "@fontsource/open-sans/700.css"
+import "@fontsource/open-sans/800.css"
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
