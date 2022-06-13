1a57c3485bc7a71285430eaa81fea626e93ae1a1
Luke Graham <luke.evan.graham@gmail.com>
2022-06-13 12:39:18 -0700

    connecting scss

--- components/Sections/FeatureRowsGroup/FeatureRowsGroup.js
+++ components/Sections/FeatureRowsGroup/FeatureRowsGroup.js
@@ -1,8 +1,12 @@
+import classes from "./FeatureRowsGroup.module.scss";
+
 const FeatureRowsGroup = ({ data }) => (
   <div>
-    {/* {console.log("DATA: ", data)} */}
+    {console.log("DATA: ", data)}
     {data.features.map((feature) => (
-      <div key={feature.id}>{feature.title}</div>
+      <div key={feature.id}>
+        <div className={classes.Title}>{feature.title}</div>
+      </div>
     ))}
   </div>
 );
