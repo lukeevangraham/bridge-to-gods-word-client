d45ee892c5cd9cb70de2158708f852f9a1386c03
Luke Graham <luke.evan.graham@gmail.com>
2023-02-05 15:08:40 -0800

    blog single breadcrumb

--- components/UI/Breadcrumb/Breadcrumb.js
+++ components/UI/Breadcrumb/Breadcrumb.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import classes from "./Breadcrumb.module.scss";
 
@@ -6,9 +7,28 @@ const Breadcrumb = ({ bgImage, title, parent }) => (
   <div className={classes.Breadcrumb}>
     <div className={classes.Breadcrumb__colorOverlay} />
     <div className={classes.Breadcrumb__BgImage}>
-      <Image src={bgImage} layout="fill" objectFit="cover" alt="Breadcrumb photo" />
+      <Image
+        src={bgImage}
+        layout="fill"
+        objectFit="cover"
+        alt="Breadcrumb photo"
+      />
     </div>
-    <h1 className={classes.Breadcrumb__title}>{title}</h1>
+    <div className={classes.Breadcrumb__text}>
+      <h1 className={classes.Breadcrumb__text__title}>{`${title}`}</h1>
+      <ul className={classes.Breadcrumb__text__parents}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        {parent ? (
+          <li>
+            <Link href={`/${parent}`}>{parent}</Link>
+          </li>
+        ) : null}
+      </ul>
+    </div>
   </div>
 );
 
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,6 @@
 import { fetchAPI, getAllBlogSlugs } from "../../lib/api";
 import Layout from "../../hoc/Layout/Layout";
+import Breadcrumb from "../../components/UI/Breadcrumb/Breadcrumb";
 
 import classes from "./slug.module.scss";
 
@@ -26,17 +27,26 @@ export async function getStaticProps({ params }) {
 
 const Blog = ({ blogData, global }) => (
   <Layout global={global}>
-    <div className={`row ${classes.BlogSingle}`}>
-      {console.log("HERE: ", blogData)}
-      <div className={classes.BlogSingle__Title}>{blogData.attributes.Title}</div>
-      <div className={classes.BlogSingle__DatePosted}>
-        {blogData.attributes.DatePosted}
+    <>
+      <Breadcrumb
+        title={blogData.attributes.Title}
+        parent={"blog"}
+        bgImage="https://res.cloudinary.com/bridge-to-god-s-word/image/upload/v1675636015/aaron_burden_x_G8_IQ_Mq_MITM_unsplash_3d9571db8a.jpg?updated_at=2023-02-05T22:26:59.780Z"
+      />
+      <div className={`row ${classes.BlogSingle}`}>
+        {console.log("HERE: ", blogData)}
+        <div className={classes.BlogSingle__Title}>
+          {blogData.attributes.Title}
+        </div>
+        <div className={classes.BlogSingle__DatePosted}>
+          {blogData.attributes.DatePosted}
+        </div>
+        <div
+          className={classes.BlogSingle__Body}
+          dangerouslySetInnerHTML={{ __html: blogData.attributes.Body }}
+        ></div>
       </div>
-      <div
-        className={classes.BlogSingle__Body}
-        dangerouslySetInnerHTML={{ __html: blogData.attributes.Body }}
-      ></div>
-    </div>
+    </>
   </Layout>
 );
 
