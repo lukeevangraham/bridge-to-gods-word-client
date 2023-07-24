655add6732ef466a70280b23f1c9f9e37b704798
Luke Graham <luke.evan.graham@gmail.com>
2023-07-24 15:17:21 -0700

    working on better home bgs

--- pages/index.js
+++ pages/index.js
@@ -56,25 +56,23 @@ export default function Home({
         <main className={classes.main}>
           {/* <h1>{homeData.data.attributes.testText}</h1> */}
 
-
-            <header className={classes.header}>
-              <div className={classes.topImage}>
-                <Image
-                  src={homeData.data.attributes.testCover.data.attributes.url}
-                  layout="fill"
-                  objectFit="cover"
-                  priority
-                  alt={
-                    homeData.data.attributes.testCover.data.attributes
-                      .alternativeText
-                  }
-                />
-              </div>
-              <Fade bottom>
-                <h1>{homeData.data.attributes.testText}</h1>
-              </Fade>
-            </header>
-
+          <header className={classes.header}>
+            <div className={classes.topImage}>
+              <Image
+                src={homeData.data.attributes.testCover.data.attributes.url}
+                layout="fill"
+                objectFit="cover"
+                priority
+                alt={
+                  homeData.data.attributes.testCover.data.attributes
+                    .alternativeText
+                }
+              />
+            </div>
+            <Fade bottom>
+              <h1>{homeData.data.attributes.testText}</h1>
+            </Fade>
+          </header>
 
           <section className={`${classes.mission} u-padding-y-medium`}>
             <div className="row">
@@ -116,203 +114,246 @@ export default function Home({
             </div>
           </section>
 
-          <section className={classes.latestBlog}>
-            <div className="row">
-              <h2 className={classes.mobileOnly}>Latest Blog</h2>
-              <div className={classes.latestBlog__container}>
-                <Fade bottom>
-                  <div className={classes.latestBlog__container__text}>
-                    <h2 className={classes.desktopOnly}>Latest Blog</h2>
+          <div className={classes.lowerSections}>
+            <section className={classes.lowerSections__latestBlog}>
+              <div className="row">
+                <h2 className={classes.mobileOnly}>
+                  Latest Blog
+                </h2>
+                <div className={classes.lowerSections__latestBlog__container}>
+                  <Fade bottom>
+                    <div
+                      className={
+                        classes.lowerSections__latestBlog__container__text
+                      }
+                    >
+                      <h2 className={classes.desktopOnly}>
+                        Latest Blog
+                      </h2>
+                      <Link href={`/blog/${latestBlog.slug}`}>
+                        <a>
+                          <h3>{latestBlog.Title}</h3>
+                        </a>
+                      </Link>
+                      <div
+                        className={
+                          classes.lowerSections__latestBlog__container__text__dateAndExcerpt
+                        }
+                      >
+                        <DateBox date={latestBlog.DatePosted} />
+                        <div
+                          className={
+                            classes.lowerSections__latestBlog__container__text__dateAndExcerpt__excerpt
+                          }
+                        >
+                          {latestBlog.Body.replace(/<br>/g, " ")
+                            .replace(/<[^>]+>/g, "")
+                            .split(" ")
+                            .splice(0, 32)
+                            .join(" ")}
+                          ...
+                        </div>
+                      </div>
+                      <div
+                        className={
+                          classes.lowerSections__latestBlog__container__text__topics
+                        }
+                      >
+                        {latestBlog.blog_topics.data.map((topic, index) => (
+                          <div key={index}>
+                            <TagCard tag={topic} />
+                          </div>
+                        ))}
+                      </div>
+                      <div
+                        className={
+                          classes.lowerSections__latestBlog__container__text__otherPosts
+                        }
+                      >
+                        <h4>
+                          <Link href="/blog">
+                            <a>See other blog posts →</a>
+                          </Link>
+                        </h4>
+                      </div>
+                    </div>
+                  </Fade>
+                  <div
+                    className={
+                      classes.lowerSections__latestBlog__container__image
+                    }
+                  >
                     <Link href={`/blog/${latestBlog.slug}`}>
                       <a>
-                        <h3>{latestBlog.Title}</h3>
+                        {latestBlog.primaryImage.data ? (
+                          <Image
+                            src={latestBlog.primaryImage.data.attributes.url}
+                            layout="fill"
+                            objectFit="cover"
+                            alt={
+                              latestBlog.primaryImage.data.attributes
+                                .alternativeText
+                            }
+                          />
+                        ) : (
+                          <Image
+                            src={
+                              "https://res.cloudinary.com/bridge-to-god-s-word/image/upload/v1677199981/hannah_olinger_8e_Sr_C43qdro_unsplash_bb1d58a63d.jpg"
+                            }
+                            layout="fill"
+                            objectFit="cover"
+                            alt="Someone writing on paper with pen"
+                          />
+                        )}
                       </a>
                     </Link>
-                    <div
-                      className={
-                        classes.latestBlog__container__text__dateAndExcerpt
-                      }
-                    >
-                      <DateBox date={latestBlog.DatePosted} />
-                      <div
-                        className={
-                          classes.latestBlog__container__text__dateAndExcerpt__excerpt
-                        }
-                      >
-                        {latestBlog.Body.replace(/<br>/g, " ")
-                          .replace(/<[^>]+>/g, "")
-                          .split(" ")
-                          .splice(0, 32)
-                          .join(" ")}
-                        ...
-                      </div>
-                    </div>
-                    <div
-                      className={classes.latestBlog__container__text__topics}
-                    >
-                      {latestBlog.blog_topics.data.map((topic, index) => (
-                        <div key={index}>
-                          <TagCard tag={topic} />
-                        </div>
-                      ))}
-                    </div>
-                    <div
-                      className={
-                        classes.latestBlog__container__text__otherPosts
-                      }
-                    >
-                      <h4>
-                        <Link href="/blog">
-                          <a>See other blog posts →</a>
-                        </Link>
-                      </h4>
-                    </div>
                   </div>
-                </Fade>
-                <div className={classes.latestBlog__container__image}>
-                  <Link href={`/blog/${latestBlog.slug}`}>
-                    <a>
-                      {latestBlog.primaryImage.data ? (
-                        <Image
-                          src={latestBlog.primaryImage.data.attributes.url}
-                          layout="fill"
-                          objectFit="cover"
-                          alt={
-                            latestBlog.primaryImage.data.attributes
-                              .alternativeText
-                          }
-                        />
-                      ) : (
-                        <Image
-                          src={
-                            "https://res.cloudinary.com/bridge-to-god-s-word/image/upload/v1677199981/hannah_olinger_8e_Sr_C43qdro_unsplash_bb1d58a63d.jpg"
-                          }
-                          layout="fill"
-                          objectFit="cover"
-                          alt="Someone writing on paper with pen"
-                        />
-                      )}
-                    </a>
-                  </Link>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section className={classes.Podcast}>
-            <div className="row">
-              <h2 className={classes.mobileOnly}>Latest Podcast</h2>
-              <div className={classes.Podcast__container}>
-                <Fade bottom>
-                  <div className={classes.Podcast__container__text}>
-                    <h2 className={classes.desktopOnly}>Latest Podcast</h2>
+            <section className={classes.lowerSections__Podcast}>
+              <div className="row">
+                <h2 className={classes.mobileOnly}>
+                  Latest Podcast
+                </h2>
+                <div className={classes.lowerSections__Podcast__container}>
+                  <Fade bottom>
+                    <div
+                      className={
+                        classes.lowerSections__Podcast__container__text
+                      }
+                    >
+                      <h2 className={classes.desktopOnly}>
+                        Latest Podcast
+                      </h2>
+                      <Link href={`/podcast#0`}>
+                        <a>
+                          <h3>{podcastData.title}</h3>
+                        </a>
+                      </Link>
+                      <div
+                        className={
+                          classes.lowerSections__Podcast__container__text__dateAndExcerpt
+                        }
+                      >
+                        <DateBox
+                          date={podcastData.pubDate}
+                          podcastDate={true}
+                        />
+                        <div
+                          className={
+                            classes.lowerSections__latestBlog__container__text__dateAndExcerpt__excerpt
+                          }
+                        >
+                          {podcastData.contentSnippet
+                            .split(" ")
+                            .splice(0, 32)
+                            .join(" ")}
+                          ...
+                        </div>
+                      </div>
+                      <div
+                        className={
+                          classes.lowerSections__latestBlog__container__text__otherPosts
+                        }
+                      >
+                        <h4>
+                          <Link href="/podcast">
+                            <a>See other podcasts →</a>
+                          </Link>
+                        </h4>
+                      </div>
+                    </div>
+                  </Fade>
+                  <div
+                    className={
+                      classes.lowerSections__latestBlog__container__image
+                    }
+                  >
                     <Link href={`/podcast#0`}>
                       <a>
-                        <h3>{podcastData.title}</h3>
+                        <Image
+                          src={podcastData.itunes.image}
+                          layout="fill"
+                          objectFit="cover"
+                          alt="Podcast Image"
+                        />
                       </a>
                     </Link>
-                    <div
-                      className={
-                        classes.Podcast__container__text__dateAndExcerpt
-                      }
-                    >
-                      <DateBox date={podcastData.pubDate} podcastDate={true} />
-                      <div
-                        className={
-                          classes.latestBlog__container__text__dateAndExcerpt__excerpt
-                        }
-                      >
-                        {podcastData.contentSnippet
-                          .split(" ")
-                          .splice(0, 32)
-                          .join(" ")}
-                        ...
-                      </div>
-                    </div>
-                    <div
-                      className={
-                        classes.latestBlog__container__text__otherPosts
-                      }
-                    >
-                      <h4>
-                        <Link href="/podcast">
-                          <a>See other podcasts →</a>
-                        </Link>
-                      </h4>
-                    </div>
                   </div>
-                </Fade>
-                <div className={classes.latestBlog__container__image}>
-                  <Link href={`/podcast#0`}>
-                    <a>
-                      <Image
-                        src={podcastData.itunes.image}
-                        layout="fill"
-                        objectFit="cover"
-                        alt="Podcast Image"
-                      />
-                    </a>
-                  </Link>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section className={classes.Newsletter}>
-            <div className="row">
-              <h2 className={classes.mobileOnly}>Latest Newsletter</h2>
-              <div className={classes.Newsletter__container}>
-                <Fade bottom>
-                  <div className={classes.Newsletter__container__text}>
-                    <h2 className={classes.desktopOnly}>Latest Newsletter</h2>
-                    <Link href={newsletter.url}>
-                      <a target="_blank">
-                        <h3>{newsletter.firstH1}</h3>
-                      </a>
-                    </Link>
+            <section className={classes.lowerSections__Newsletter}>
+              <div className="row">
+                <h2 className={classes.mobileOnly}>
+                  Latest Newsletter
+                </h2>
+                <div className={classes.lowerSections__Newsletter__container}>
+                  <Fade bottom>
                     <div
                       className={
-                        classes.Newsletter__container__text__dateAndExcerpt
+                        classes.lowerSections__Newsletter__container__text
                       }
                     >
-                      <DateBox date={newsletter.date} />
+                      <h2 className={classes.desktopOnly}>
+                        Latest Newsletter
+                      </h2>
+                      <Link href={newsletter.url}>
+                        <a target="_blank">
+                          <h3>{newsletter.firstH1}</h3>
+                        </a>
+                      </Link>
                       <div
                         className={
-                          classes.Newsletter__container__text__dateAndExcerpt__excerpt
+                          classes.lowerSections__Newsletter__container__text__dateAndExcerpt
                         }
                       >
-                        {newsletter.secondText}
-                        ...
+                        <DateBox date={newsletter.date} />
+                        <div
+                          className={
+                            classes.lowerSections__Newsletter__container__text__dateAndExcerpt__excerpt
+                          }
+                        >
+                          {newsletter.secondText}
+                          ...
+                        </div>
+                      </div>
+                      <div
+                        className={
+                          classes.lowerSections__Newsletter__container__text__otherPosts
+                        }
+                      >
+                        <h4>
+                          <Link href="https://us6.campaign-archive.com/home/?u=6bac04db4f991f5af4f84dabb&id=3606fc3bbc">
+                            <a target="_blank">See other newsletters →</a>
+                          </Link>
+                        </h4>
                       </div>
                     </div>
-                    <div
-                      className={
-                        classes.Newsletter__container__text__otherPosts
-                      }
-                    >
-                      <h4>
-                        <Link href="https://us6.campaign-archive.com/home/?u=6bac04db4f991f5af4f84dabb&id=3606fc3bbc">
-                          <a target="_blank">See other newsletters →</a>
-                        </Link>
-                      </h4>
-                    </div>
+                  </Fade>
+                  <div
+                    className={
+                      classes.lowerSections__latestBlog__container__image
+                    }
+                  >
+                    <Link href={newsletter.url}>
+                      <a>
+                        <Image
+                          src={newsletter.topImage}
+                          layout="fill"
+                          objectFit="cover"
+                          alt="Newsletter image"
+                        />
+                      </a>
+                    </Link>
                   </div>
-                </Fade>
-                <div className={classes.latestBlog__container__image}>
-                  <Link href={newsletter.url}>
-                    <a>
-                      <Image
-                        src={newsletter.topImage}
-                        layout="fill"
-                        objectFit="cover"
-                        alt="Newsletter image"
-                      />
-                    </a>
-                  </Link>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
 
           {/* <Sections sections={homeData.data.attributes.contentSections} /> */}
         </main>
