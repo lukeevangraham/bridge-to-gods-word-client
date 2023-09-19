e68edf218fdbb94013adf31027a8406f3f060ab9
Luke Graham <luke.evan.graham@gmail.com>
2023-09-18 22:48:07 -0700

    google tag

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,25 @@ import "@fontsource/open-sans/600.css";
 import "@fontsource/open-sans/700.css";
 import "@fontsource/open-sans/800.css";
 import { ParallaxProvider } from "react-scroll-parallax";
+import Script from "next/script";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
     <ParallaxProvider>
       <Component {...pageProps} />
     </ParallaxProvider>
+    {/* <!-- Google tag (gtag.js) --> */}
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-R9P4ZTM9L6" />
+<Script>
+  {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-R9P4ZTM9L6');`}
+</Script>
+    </>
   );
 }
 
